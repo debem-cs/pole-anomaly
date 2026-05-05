ed495cec61ee66c04543781224deb2bf3f43b43d
Pedro <pedro.debem@student-cs.fr>
2026-05-05 09:06:31 +0200

    test deformation with same parameters as the generated dataset

--- data-gen/script/test_generator_deformation.py
+++ data-gen/script/test_generator_deformation.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import glob
+import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -29,8 +30,13 @@ def test_anomaly_deformation():
         df = pd.read_csv(template_path)
         
         # Test generations straight from the template to visualize deformation randomness
-        for j in range(5):
-            t, v = generate_anomaly(df, amplitude=50, period=200, variance=0.02)
+        for j in range(6):
+            std_noise = 4.36
+            target_amplitude = std_noise * np.random.uniform(3.0, 7.6)
+            target_period = np.random.randint(200, 500)
+            variance_level = np.random.uniform(0.02, 0.08)
+
+            t, v = generate_anomaly(df, amplitude=target_amplitude, period=target_period, variance=variance_level)
             fig.add_trace(go.Scatter(
                 x=t, 
                 y=v, 
